b264639634bc107bb59410f85b7f079e5d58f0a8
James Delancey <jamesdelanceyjr@gmail.com>
2022-03-19 16:33:50 -0700

    Update apply.py

--- modules/emailcli/apply.py
+++ modules/emailcli/apply.py
@@ -8,6 +8,10 @@ local_repo_path = os.environ.get("EMAILCLI_REPO_PATH")
 os.makedirs(os.path.dirname(local_repo_path), exist_ok=True)
 local_repo_python_entrypoint_long_fn = local_repo_path + "/emailcli/main.py"
 
+
+timer_unit_file_full_fn = "/etc/systemd/system/ccam.timer"
+service_unit_file_full_fn = "/etc/systemd/system/ccam.service"
+
 if not os.path.exists(local_repo_path):
     subprocess.check_output(["git", "clone", repo_url, local_repo_path])
 
@@ -23,3 +27,31 @@ git_output = subprocess.check_output(
     ["git", "-C", local_repo_path, "pull"]
 )
 logging.debug("git pull output: %s", git_output.decode())
+
+# if not os.path.exists(timer_unit_file_full_fn):
+#     git_output = subprocess.check_output(
+#         ["systemctl", "daemon-reload"]
+#     )
+#     logging.debug("systemctl daemon-reload : %s", git_output.decode())
+#     git_output = subprocess.check_output(
+#         ["systemctl", "enable", "ccam.service"]
+#     )
+#     logging.debug("systemctl enable ccam.service: %s", git_output.decode())
+#     git_output = subprocess.check_output(
+#         ["systemctl", "start", "ccam.service"]
+#     )
+#     logging.debug("systemctl start ccam.service: %s", git_output.decode())
+#     git_output = subprocess.check_output(
+#         ["systemctl", "status", "ccam.service"]
+#     )
+#     logging.debug("systemctl status ccam.service: %s", git_output.decode())
+
+# if "Already up to date." not in git_output.decode():
+#     git_output = subprocess.check_output(
+#         ["systemctl", "daemon-reload"]
+#     )
+#     logging.debug("systemctl daemon-reload : %s", git_output.decode())
+#     git_output = subprocess.check_output(
+#         ["systemctl", "restart", "ccam.service"]
+#     )
+#     logging.debug("systemctl restart ccam.service: %s", git_output.decode())
