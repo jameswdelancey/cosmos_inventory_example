07833b43688891cb78dacb846536d4b89b89f157
James Delancey <jamesdelanceyjr@gmail.com>
2022-03-19 12:15:57 -0700

    auto commit

---
+++ modules/emailcli/apply.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+repo_url = os.environ.get("EMAILCLI_REPO")
+sqlite_path = os.environ.get("EMAILCLI_SQLITE_PATH")
+os.makedirs(sqlite_path, exist_ok=True)
+local_repo_path = os.environ.get("EMAILCLI_REPO_PATH")
+local_repo_python_entrypoint_long_fn = local_repo_path + "/emailcli/main.py"
+
+if not os.path.exists(local_repo_path):
+    subprocess.check_output(["git", "clone", repo_url, local_repo_path])
+
+# subprocess.check_output(["git", "-C", local_repo_path, "pull"])
