56aa5a240c58b71e36b3dc790789374b0f386e72
James Delancey <jamesdelanceyjr@gmail.com>
2022-03-19 12:18:29 -0700

    auto commit

--- modules/emailcli/apply.py
+++ modules/emailcli/apply.py
@@ -5,6 +5,7 @@ repo_url = os.environ.get("EMAILCLI_REPO")
 sqlite_path = os.environ.get("EMAILCLI_SQLITE_PATH")
 os.makedirs(sqlite_path, exist_ok=True)
 local_repo_path = os.environ.get("EMAILCLI_REPO_PATH")
+os.makedirs(os.path.dirname(local_repo_path), exist_ok=True)
 local_repo_python_entrypoint_long_fn = local_repo_path + "/emailcli/main.py"
 
 if not os.path.exists(local_repo_path):
