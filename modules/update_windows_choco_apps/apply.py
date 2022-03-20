3cb4ee2c651e457f97b1b11c09765c733adbf6d4
James Delancey <jamesdelanceyjr@gmail.com>
2022-03-20 12:41:34 -0700

    Update apply.py

---
+++ modules/update_windows_choco_apps/apply.py
@@ -0,0 +1,12 @@
+import subprocess
+# import os
+import logging
+import datetime
+
+
+out = subprocess.check_output(["choco", "feature", "enable", "-n=allowGlobalConfirmation"])
+logging.debug("choco feature enable allowGlobalConfirmation output: %s", out.decode())
+
+if datetime.datetime.now().hour == 12:
+    out = subprocess.check_output(["choco", "update", "all"])
+    logging.debug("choco upgrade all output: %s", out.decode())
