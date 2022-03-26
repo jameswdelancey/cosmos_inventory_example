254ff5b17b2c1d60e1a0d0d9a5795a695a98a1b6
James Delancey <jamesdelanceyjr@gmail.com>
2022-03-26 09:24:31 -0700

    Update drop.py

---
+++ modules/timeseries_server_core/drop.py
@@ -0,0 +1,17 @@
+import os
+import logging
+import subprocess
+
+repo_url = os.environ.get("TIMESERIES_SERVER_REPO")
+sqlite_path = os.environ.get("TIMESERIES_SERVER_SQLITE_PATH") # NOT FULL FILENAME
+# os.makedirs(sqlite_path, exist_ok=True)
+local_repo_path = os.environ.get("TIMESERIES_SERVER_REPO_PATH")
+# os.makedirs(os.path.dirname(local_repo_path), exist_ok=True)
+local_repo_python_entrypoint_long_fn = local_repo_path + "/timeseries_server/main.py"
+
+
+try:
+    shutil.rmtree(local_repo_path)
+    logging.info("removed local repo at path: %s", local_repo_path)
+except Exception as e:
+    logging.exception("could not remove local repo at path: %s, with error: %s", local_repo_path, repr(e))
