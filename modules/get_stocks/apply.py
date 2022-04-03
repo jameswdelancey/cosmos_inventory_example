2bbdf63bb7c0df0c5880668939b9192a6cb1a057
James Delancey <jamesdelanceyjr@gmail.com>
2022-04-02 20:30:08 -0700

    add config repo

--- modules/get_stocks/apply.py
+++ modules/get_stocks/apply.py
@@ -8,6 +8,7 @@ os.makedirs(config_path, exist_ok=True)
 sqlite_path = os.environ.get("GET_STOCKS_DATA_DIR")  # NOT FULL FILENAME
 os.makedirs(sqlite_path, exist_ok=True)
 local_repo_path = os.environ.get("GET_STOCKS_REPO_PATH")
+config_repo = os.environ.get("GET_STOCKS_CONFIG_REPO")
 os.makedirs(os.path.dirname(local_repo_path), exist_ok=True)
 local_repo_python_entrypoint_long_fn = local_repo_path + "/get_stocks/main.py"
 
@@ -43,73 +44,71 @@ local_repo_python_entrypoint_long_fn = local_repo_path + "/get_stocks/main.py"
 # for fn in FILENAMES_FOR_UNITFILES:
 #     unitfile_fullpaths.append("%s/%s" % (PATH_FOR_UNITFILE, fn))
 
+for _path, _url in [[local_repo_path, repo_url], [config_repo, config_path]]:
+    if not os.path.exists(_path):
+        git_output = subprocess.check_output(["git", "clone", _url, _path])
+        logging.debug("git clone output: %s", git_output.decode())
+    else:
+        git_output = subprocess.check_output(["git", "-C", _path, "reset", "--hard"])
+        logging.debug("git reset output: %s", git_output.decode())
+        git_output = subprocess.check_output(["git", "-C", _path, "clean", "-fd"])
+        logging.debug("git clean output: %s", git_output.decode())
+        git_output = subprocess.check_output(["git", "-C", _path, "pull"])
+        logging.debug("git pull output: %s", git_output.decode())
 
-if not os.path.exists(local_repo_path):
-    git_output = subprocess.check_output(["git", "clone", repo_url, local_repo_path])
-    logging.debug("git clone output: %s", git_output.decode())
-else:
-    git_output = subprocess.check_output(
-        ["git", "-C", local_repo_path, "reset", "--hard"]
-    )
-    logging.debug("git reset output: %s", git_output.decode())
-    git_output = subprocess.check_output(["git", "-C", local_repo_path, "clean", "-fd"])
-    logging.debug("git clean output: %s", git_output.decode())
-    git_output = subprocess.check_output(["git", "-C", local_repo_path, "pull"])
-    logging.debug("git pull output: %s", git_output.decode())
+    # update local if repo changed
+    repo_changed = "Already up to date." not in git_output.decode()
 
+    if repo_changed:
+        os.chdir(local_repo_path)
 
-# update local if repo changed
-repo_changed = "Already up to date." not in git_output.decode()
+        # install poetry
+        COMMANDS_TO_RUN = [
+            ["apt", "install", "-y", "python3-pip"],
+            ["pip3", "install", "poetry"],
+        ]
+        for command in COMMANDS_TO_RUN:
+            logging.info("running command to install poetry %s", repr(command))
+            subprocess.run(command)
 
-if repo_changed:
-    os.chdir(local_repo_path)
+        # refresh poetry requirements
+        COMMANDS_TO_RUN = [
+            ["poetry", "install"],
+            ["chown", "-R", "pi:pi", local_repo_path],
+            ["chown", "-R", "pi:pi", config_repo_path],
+            ["chown", "-R", "pi:pi", sqlite_path],
+        ]
+        for command in COMMANDS_TO_RUN:
+            logging.info("running command to refresh poetry %s", repr(command))
+            subprocess.check_output(command)
 
-    # install poetry
-    COMMANDS_TO_RUN = [
-        ["apt", "install", "-y", "python3-pip"],
-        ["pip3", "install", "poetry"],
-    ]
-    for command in COMMANDS_TO_RUN:
-        logging.info("running command to install poetry %s", repr(command))
-        subprocess.run(command)
+        # # reapply unitfile
+        # for unitfile_fullpath, payload in zip(unitfile_fullpaths, UNIT_FILE_PAYLOADS):
+        #     try:
+        #         logging.info("creating unitfile at path %s", unitfile_fullpath)
+        #         with open(unitfile_fullpath, "w") as f:
+        #             f.write(payload)
+        #     except Exception as e:
+        #         logging.exception(
+        #             "error creating unitfile at path %s with error %s",
+        #             unitfile_fullpath,
+        #             repr(e),
+        #         )
 
-    # refresh poetry requirements
-    COMMANDS_TO_RUN = [
-        ["poetry", "install"],
-        ["chown", "-R", "pi:pi", local_repo_path],
-        ["chown", "-R", "pi:pi", sqlite_path],
-    ]
-    for command in COMMANDS_TO_RUN:
-        logging.info("running command to refresh poetry %s", repr(command))
-        subprocess.check_output(command)
+        # # refresh systemd daemon
+        # COMMANDS_TO_RUN = [
+        #     ["systemctl", "daemon-reload"],
+        # ]
+        # for fn in FILENAMES_FOR_UNITFILES:
+        #     COMMANDS_TO_RUN.extend(
+        #         [["systemctl", "restart", fn]]
+        #     )
+        # for command in COMMANDS_TO_RUN:
+        #     logging.info("running command to refresh systemd daemon %s", repr(command))
+        #     subprocess.run(command)
 
-    # # reapply unitfile
-    # for unitfile_fullpath, payload in zip(unitfile_fullpaths, UNIT_FILE_PAYLOADS):
-    #     try:
-    #         logging.info("creating unitfile at path %s", unitfile_fullpath)
-    #         with open(unitfile_fullpath, "w") as f:
-    #             f.write(payload)
-    #     except Exception as e:
-    #         logging.exception(
-    #             "error creating unitfile at path %s with error %s",
-    #             unitfile_fullpath,
-    #             repr(e),
-    #         )
-
-    # # refresh systemd daemon
-    # COMMANDS_TO_RUN = [
-    #     ["systemctl", "daemon-reload"],
-    # ]
-    # for fn in FILENAMES_FOR_UNITFILES:
-    #     COMMANDS_TO_RUN.extend(
-    #         [["systemctl", "restart", fn]]
-    #     )
-    # for command in COMMANDS_TO_RUN:
-    #     logging.info("running command to refresh systemd daemon %s", repr(command))
-    #     subprocess.run(command)
-
-    # # restart service
-    # COMMANDS_TO_RUN = [["systemctl", "restart", fn] for fn in FILENAMES_FOR_UNITFILES]
-    # for command in COMMANDS_TO_RUN:
-    #     logging.info("running command to restart services %s", repr(command))
-    #     subprocess.check_output(command)
+        # # restart service
+        # COMMANDS_TO_RUN = [["systemctl", "restart", fn] for fn in FILENAMES_FOR_UNITFILES]
+        # for command in COMMANDS_TO_RUN:
+        #     logging.info("running command to restart services %s", repr(command))
+        #     subprocess.check_output(command)
