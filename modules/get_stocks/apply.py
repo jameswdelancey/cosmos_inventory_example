f5b0f1bee5018bfb33ef8ae640ac7422c907032b
James Delancey <jamesdelanceyjr@gmail.com>
2022-04-02 20:53:38 -0700

    add get_stocks service unitfile

--- modules/get_stocks/apply.py
+++ modules/get_stocks/apply.py
@@ -112,3 +112,98 @@ for _path, _url in [[local_repo_path, repo_url], [config_path, config_repo]]:
         # for command in COMMANDS_TO_RUN:
         #     logging.info("running command to restart services %s", repr(command))
         #     subprocess.check_output(command)
+
+SERVER_NAMES = ["refresh_policies"]
+UNIT_FILE_PAYLOADS = []
+for server in SERVER_NAMES:
+    UNIT_FILE_PAYLOADS.append(
+        """\
+[Unit]
+Description=Get Stocks Server %s
+StartLimitInterval=400
+StartLimitBurst=5
+[Service]
+Type=simple
+EnvironmentFile=/etc/environment
+WorkingDirectory=%s
+ExecStart=/usr/local/bin/poetry run python '%s' '%s'
+Restart=always
+RestartSec=30
+# WatchdogSec=60
+KillMode=process
+User=pi
+Group=pi
+[Install]
+WantedBy=multi-user.target
+"""
+        % (server, local_repo_path, local_repo_python_entrypoint_long_fn, server)
+    )
+FILENAMES_FOR_UNITFILES = [f"{server}.service" for server in SERVER_NAMES]
+PATH_FOR_UNITFILE = "/etc/systemd/system"
+
+unitfile_fullpaths = []
+for fn in FILENAMES_FOR_UNITFILES:
+    unitfile_fullpaths.append("%s/%s" % (PATH_FOR_UNITFILE, fn))
+#####
+CURRENT_PAYLOADS = UNIT_FILE_PAYLOADS
+PAST_PAYLOADS = []
+for fn in unitfile_fullpaths:
+    try:
+        with open(fn) as f:
+            PAST_PAYLOADS.append(f.read())
+    except Exception as e:
+        PAST_PAYLOADS.append("")
+repo_changed = CURRENT_PAYLOADS != PAST_PAYLOADS
+logging.info("repo_chaged is %s", repo_changed)
+if repo_changed:
+    os.chdir(local_repo_path)
+
+    # # install poetry
+    # COMMANDS_TO_RUN = [
+    #     ["apt", "install", "-y", "python3-pip"],
+    #     ["pip3", "install", "poetry"],
+    # ]
+    # for command in COMMANDS_TO_RUN:
+    #     logging.info("running command to install poetry %s", repr(command))
+    #     subprocess.run(command)
+
+    # refresh poetry requirements
+    COMMANDS_TO_RUN = [
+        # ["poetry", "install"],
+        # ["chown", "-R", "pi:pi", local_repo_path],
+        # ["chown", "-R", "pi:pi", sqlite_path],
+    ]
+    for command in COMMANDS_TO_RUN:
+        logging.info("running command to refresh poetry %s", repr(command))
+        subprocess.check_output(command)
+
+    # reapply unitfile
+    for unitfile_fullpath, payload in zip(unitfile_fullpaths, UNIT_FILE_PAYLOADS):
+        try:
+            logging.info("creating unitfile at path %s", unitfile_fullpath)
+            with open(unitfile_fullpath, "w") as f:
+                f.write(payload)
+        except Exception as e:
+            logging.exception(
+                "error creating unitfile at path %s with error %s",
+                unitfile_fullpath,
+                repr(e),
+            )
+
+    # refresh systemd daemon
+    COMMANDS_TO_RUN = [
+        ["systemctl", "daemon-reload"],
+    ]
+    for fn in FILENAMES_FOR_UNITFILES:
+        COMMANDS_TO_RUN.extend(
+            [["systemctl", "enable", fn], ["systemctl", "start", fn]]
+        )
+    for command in COMMANDS_TO_RUN:
+        logging.info("running command to refresh systemd daemon %s", repr(command))
+        subprocess.check_output(command)
+
+    # restart service
+    COMMANDS_TO_RUN = [["systemctl", "restart", fn] for fn in FILENAMES_FOR_UNITFILES]
+    for command in COMMANDS_TO_RUN:
+        logging.info("running command to restart services %s", repr(command))
+        subprocess.check_output(command)
