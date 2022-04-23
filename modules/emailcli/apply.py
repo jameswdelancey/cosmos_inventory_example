e0504986ee076c5d0585577b4c6b0cb1cc509743
James Delancey <jamesdelanceyjr@gmail.com>
2022-04-23 13:43:17 -0700

    Update apply.py

--- modules/emailcli/apply.py
+++ modules/emailcli/apply.py
@@ -6,49 +6,99 @@ repo_url = os.environ.get("EMAILCLI_REPO")
 sqlite_path = os.environ.get("EMAILCLI_SQLITE_PATH")  # NOT FULL FILENAME
 os.makedirs(sqlite_path, exist_ok=True)
 local_repo_path = os.environ.get("EMAILCLI_REPO_PATH")
-os.makedirs(os.path.dirname(local_repo_path), exist_ok=True)
+# os.makedirs(os.path.dirname(local_repo_path), exist_ok=True)
 local_repo_python_entrypoint_long_fn = local_repo_path + "/emailcli/main.py"
 
-# I dont want to use systemd for everything because it will not
-# work on windows but we can do something that works on both
-#
-# timer_unit_file_full_fn = "/etc/systemd/system/ccam.timer"
-# service_unit_file_full_fn = "/etc/systemd/system/ccam.service"
+SERVER_NAMES = []
+UNIT_FILE_PAYLOADS = []
+for server in SERVER_NAMES:
+    UNIT_FILE_PAYLOADS.append(
+        """\
+[Unit]
+Description=EmailCLI Server %s
+StartLimitInterval=400
+StartLimitBurst=5
+[Service]
+Type=simple
+EnvironmentFile=/etc/environment
+WorkingDirectory=%s
+ExecStart=/usr/local/bin/poetry run python '%s' '%s'
+Restart=always
+RestartSec=30
+# WatchdogSec=60
+KillMode=process
+User=pi
+Group=pi
+[Install]
+WantedBy=multi-user.target
+"""
+        % (server, local_repo_path, local_repo_python_entrypoint_long_fn, server)
+    )
+FILENAMES_FOR_UNITFILES = [f"{server}.service" for server in SERVER_NAMES]
+PATH_FOR_UNITFILE = "/etc/systemd/system"
+
+unitfile_fullpaths = []
+for fn in FILENAMES_FOR_UNITFILES:
+    unitfile_fullpaths.append("%s/%s" % (PATH_FOR_UNITFILE, fn))
 
 if not os.path.exists(local_repo_path):
-    subprocess.check_output(["git", "clone", repo_url, local_repo_path])
+    git_output = subprocess.check_output(["git", "clone", repo_url, local_repo_path])
+    logging.debug("git clone output: %s", git_output.decode())
+else:
+    git_output = subprocess.check_output(
+        ["git", "-C", local_repo_path, "reset", "--hard"]
+    )
+    logging.debug("git reset output: %s", git_output.decode())
+    git_output = subprocess.check_output(["git", "-C", local_repo_path, "clean", "-fd"])
+    logging.debug("git clean output: %s", git_output.decode())
+    git_output = subprocess.check_output(["git", "-C", local_repo_path, "pull"])
+    logging.debug("git pull output: %s", git_output.decode())
 
-git_output = subprocess.check_output(["git", "-C", local_repo_path, "reset", "--hard"])
-logging.debug("git reset output: %s", git_output.decode())
-git_output = subprocess.check_output(["git", "-C", local_repo_path, "clean", "-fd"])
-logging.debug("git clean output: %s", git_output.decode())
-git_output = subprocess.check_output(["git", "-C", local_repo_path, "pull"])
-logging.debug("git pull output: %s", git_output.decode())
+# update local if repo changed
+repo_changed = "Already up to date." not in git_output.decode()
 
-# if not os.path.exists(timer_unit_file_full_fn):
-#     git_output = subprocess.check_output(
-#         ["systemctl", "daemon-reload"]
-#     )
-#     logging.debug("systemctl daemon-reload : %s", git_output.decode())
-#     git_output = subprocess.check_output(
-#         ["systemctl", "enable", "ccam.service"]
-#     )
-#     logging.debug("systemctl enable ccam.service: %s", git_output.decode())
-#     git_output = subprocess.check_output(
-#         ["systemctl", "start", "ccam.service"]
-#     )
-#     logging.debug("systemctl start ccam.service: %s", git_output.decode())
-#     git_output = subprocess.check_output(
-#         ["systemctl", "status", "ccam.service"]
-#     )
-#     logging.debug("systemctl status ccam.service: %s", git_output.decode())
+if repo_changed:
+    os.chdir(local_repo_path)
+    # install poetry
+    COMMANDS_TO_RUN = [
+        ["apt", "install", "-y", "python3-pip"],
+        ["pip3", "install", "poetry"],
+        ["poetry", "install"],
+        ["chown", "-R", "pi:pi", local_repo_path],
+        ["chown", "-R", "pi:pi", sqlite_path],
+    ]
+    for command in COMMANDS_TO_RUN:
+        logging.info("running command to install poetry %s", repr(command))
+        subprocess.run(command)
 
-# if "Already up to date." not in git_output.decode():
-#     git_output = subprocess.check_output(
-#         ["systemctl", "daemon-reload"]
-#     )
-#     logging.debug("systemctl daemon-reload : %s", git_output.decode())
-#     git_output = subprocess.check_output(
-#         ["systemctl", "restart", "ccam.service"]
-#     )
-#     logging.debug("systemctl restart ccam.service: %s", git_output.decode())
+    # reapply unitfile
+    for unitfile_fullpath, payload in zip(unitfile_fullpaths, UNIT_FILE_PAYLOADS):
+        try:
+            logging.info("creating unitfile at path %s", unitfile_fullpath)
+            with open(unitfile_fullpath, "w") as f:
+                f.write(payload)
+        except Exception as e:
+            logging.exception(
+                "error creating unitfile at path %s with error %s",
+                unitfile_fullpath,
+                repr(e),
+            )
+
+
+    # refresh systemd daemon
+    COMMANDS_TO_RUN = [
+        ["systemctl", "daemon-reload"],
+    ]
+    for fn in FILENAMES_FOR_UNITFILES:
+        COMMANDS_TO_RUN.extend(
+            [["systemctl", "enable", fn], ["systemctl", "start", fn]]
+        )
+    for command in COMMANDS_TO_RUN:
+        logging.info("running command to refresh systemd daemon %s", repr(command))
+        subprocess.check_output(command)
+
+    # restart service
+    COMMANDS_TO_RUN = [["systemctl", "restart", fn] for fn in FILENAMES_FOR_UNITFILES]
+    for command in COMMANDS_TO_RUN:
+        logging.info("running command to restart services %s", repr(command))
+        subprocess.check_output(command)
